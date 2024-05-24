898167bfba9c90e1d5b59e7fb8850d36a2a06d79
dashlexz1 <adr1999@icloud.com>
2024-05-24 15:42:02 +0300

    restClients

---
+++ REST-CLIENTS 3.1.6/src/test/java/com/dashlexz/spring/rest_clients/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashlexz.spring.rest_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
